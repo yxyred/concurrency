d476d260663743d53fcaa3e36cb7323bb2c2e398
yxy <731581233@qq.com>
2018-05-18 17:21:48 +0800

    Atomic

--- src/main/java/com/mmal/concurrency/Atomic/AtomicInt.java
+++ src/main/java/com/mmal/concurrency/Atomic/AtomicInt.java
@@ -1,19 +1,21 @@
-package com.mmal.concurrency;
+package com.mmal.concurrency.Atomic;
 
 import com.mmal.concurrency.annotation.NotThreadSafe;
+import com.mmal.concurrency.annotation.ThreadSafe;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
-@NotThreadSafe
-public class mytest {
+@ThreadSafe
+public class AtomicInt {
     public static int ClientTotal = 5000;
     public static int ThreadTotal =200;
-    public static int count=0;
+    public static AtomicInteger count = new AtomicInteger(0);
     public static void main(String[] args) throws  Exception{
         ExecutorService executorService = Executors.newCachedThreadPool();
         final Semaphore semaphore = new Semaphore(ThreadTotal);
@@ -32,9 +34,9 @@ public class mytest {
         }
         countDownLatch.await();
         executorService.shutdown();
-        log.info("count:{}",count);
+        log.info("count:{}",count.get());
     }
     public static void add(){
-        count ++;
+        count.incrementAndGet();
     }
 }
