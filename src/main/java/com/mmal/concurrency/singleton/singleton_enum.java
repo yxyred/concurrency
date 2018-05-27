6b2a8bba32acd77aa08a86f9328afe71df25f9bd
yxy <731581233@qq.com>
2018-05-27 16:44:09 +0800

    cccccc

--- src/main/java/com/mmal/concurrency/singleton/singleton_enum.java
+++ src/main/java/com/mmal/concurrency/singleton/singleton_enum.java
@@ -6,7 +6,7 @@ import com.mmal.concurrency.annotation.ThreadSafe;
 
 
 /*
-* 枚举方法*/
+* 枚举模式，最安全的，不需要其他的方式去维护*/
 @ThreadSafe
 @Recommend
 public class singleton_enum {
@@ -21,6 +21,7 @@ public class singleton_enum {
     private enum single{
         INSTANCE;
         private singleton_enum singletonEnum;
+        /*jvm保证这个方法只使用了一次*/
         single(){
             singletonEnum = new singleton_enum();
         }
